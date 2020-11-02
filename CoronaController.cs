64eef953315f848a941c1c0aaf108afb04930ada
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2020-11-02 12:03:05 +0100

    edit CoronaController and CoronaOperations
    use dapper instead of ado.net

---
+++ CoronaController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class CoronaController : ApiController
+    {
+        // GET: /Corona
+        [Route("Corona")]
+        [HttpGet]
+        public List<Datum> Get()
+        {
+            CoronaOperations coop = new CoronaOperations();
+            return coop.GetTheRecords(100, "ASC");
+        }
+
+        /*
+        // GET: api/Corona
+        public List<Datum> Get(string country)
+        {
+            CoronaOperations coop = new CoronaOperations();
+            return coop.GetTheRecords(string.Format("SELECT * FROM theStats WHERE countrycode = '{0}'", country));
+        }
+        */
+        // GET: api/Corona/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: /Corona
+        [Route("Corona")]
+        [HttpPost]
+        // POST: api/Corona
+        public void Post([FromBody]Datum datum)
+        {
+            CoronaOperations coop = new CoronaOperations();
+            coop.InsertRecord(datum);
+        }
+
+        // PUT: api/Corona/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Corona/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
