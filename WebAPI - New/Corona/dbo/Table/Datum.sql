8db212d5e010a3dcc1cf74cb9e993daff915fed5
Yi-Chen Lin <1081501@ucn.dk>
2020-11-02 13:16:38 +0100

    change CoronaController, CoronaOperations: use dapper instead, add NuGet: dapper, add connection string in Web.config

---
+++ WebAPI - New/Corona/dbo/Table/Datum.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Datum]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITy(1, 1),
+    [CountryCode] NCHAR(50) NULL, 
+    [Date] NVARCHAR(50) NULL, 
+    [Cases] NVARCHAR(50) NULL, 
+    [Deaths] NVARCHAR(50) NULL, 
+    [Recovered] NVARCHAR(50) NULL
+)
